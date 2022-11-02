fe453b9d22d0c40958e2093cea690188d1f05e3f
Fnisnomore <Tomeelman33@gmail.com>
2022-11-02 14:05:35 +0100

    Walk

--- 2D-GAME/Assets/Scripts/PlayerMovement.cs
+++ 2D-GAME/Assets/Scripts/PlayerMovement.cs
@@ -1,21 +1,34 @@
-
 using UnityEngine;
  
 public class PlayerMovement : MonoBehaviour
-{ 
+{
     [SerializeField] private float speed;
+    [SerializeField] private float sprint;
     private Rigidbody2D body;
- 
+    private Animator anim;
+    private bool grounded;
+    
     private void Awake()
-    {
+    {   
+        //Grabs references for rigidbody and animator from game object.
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
  
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
- 
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+        if (Input.GetKey(KeyCode.LeftShift)) body.velocity = new Vector2(Input.GetAxis("Horizontal") * sprint, body.velocity.y);
+
         if (Input.GetKey(KeyCode.Space))
             body.velocity = new Vector2(body.velocity.x, speed);
+ 
+        //Flip player when facing left/right.
+        if (horizontalInput > 0.01f)
+            transform.localScale = new Vector3(2, 2, 1);
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-2, 2, 1);
+ 
     }
 }
