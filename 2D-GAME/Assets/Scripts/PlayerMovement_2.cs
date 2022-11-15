1ad3a17bc0b7cf1220e0f71487e247a5a2d67a1a
Joost-berkhout <260422@cursist.rockopnh.nl>
2022-11-15 12:25:00 +0100

    Update PlayerMovement_2.cs

--- 2D-GAME/Assets/Scripts/PlayerMovement_2.cs
+++ 2D-GAME/Assets/Scripts/PlayerMovement_2.cs
@@ -1,35 +1,47 @@
+
 using UnityEngine;
  
 public class PlayerMovement_2 : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private float sprint;
-    private Rigidbody2D body;
-    private Animator anim;
+
+    float MovementX;
+    float MovementY;
     
-    private void Awake()
-    {   
-        //Grabs references for rigidbody and animator from game object.
+    Rigidbody2D body;
+    private void Start()
+    {
         body = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
+
+        MovementX = 0;
+        MovementY = 0;
     }
  
     private void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
-        if (Input.GetKey(KeyCode.LeftShift)) body.velocity = new Vector2(Input.GetAxis("Horizontal") * sprint, body.velocity.y);
 
-        if (Input.GetKey(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x, speed);
- 
-        //Flip player when facing left/right.
-        if (horizontalInput > 0.01f)
-            transform.localScale = new Vector3(2, 2, 1);
-        else if (horizontalInput < -0.01f)
-            transform.localScale = new Vector3(-2, 2, 1);
+        body.velocity = new Vector2(MovementX * speed * Time.deltaTime, MovementY * speed * Time.deltaTime);
+        Debug.Log(MovementX + " " + MovementY);
 
-                anim.SetBool("Walk", horizontalInput != 0);
- 
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            MovementY = 1;
+        }
+            if (Input.GetKey(KeyCode.DownArrow))
+        {
+            MovementY = -1;
+        }
+            if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            MovementX = 1;
+        }
+            if (Input.GetKey(KeyCode.RightArrow))
+        {
+            MovementX = 1;
+        }
+
+
+        
     }
 }
