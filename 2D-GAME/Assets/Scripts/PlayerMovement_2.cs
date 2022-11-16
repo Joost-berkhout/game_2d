a5ce5c339b9081d2ce534c20612fa9d2ca60004d
Joost-berkhout <260422@cursist.rockopnh.nl>
2022-11-16 11:09:22 +0100

    player movement_2

--- 2D-GAME/Assets/Scripts/PlayerMovement_2.cs
+++ 2D-GAME/Assets/Scripts/PlayerMovement_2.cs
@@ -5,41 +5,36 @@ public class PlayerMovement_2 : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private float sprint;
+    [SerializeField] private float Jump;
+    private Rigidbody2D body;
 
     float MovementX;
     float MovementY;
     
-    Rigidbody2D body;
     private void Start()
     {
         body = GetComponent<Rigidbody2D>();
 
-        MovementX = 0;
-        MovementY = 0;
     }
  
     private void Update()
     {
-
-        body.velocity = new Vector2(MovementX * speed * Time.deltaTime, MovementY * speed * Time.deltaTime);
+        body.velocity = new Vector2(MovementX * speed, body.velocity.y);   
+        
         Debug.Log(MovementX + " " + MovementY);
 
+            if (Input.GetKeyDown (KeyCode.LeftArrow))
+        {
+            MovementX = -speed;
+        }
+            if (Input.GetKeyDown (KeyCode.RightArrow))
+        {
+            MovementX = speed;
+        }
         if (Input.GetKey(KeyCode.UpArrow))
-        {
-            MovementY = 15;
-        }
-            if (Input.GetKey(KeyCode.DownArrow))
-        {
-            MovementY = -15;
-        }
-            if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            MovementX = -15;
-        }
-            if (Input.GetKey(KeyCode.RightArrow))
-        {
-            MovementX = 15;
-        }
-
+            body.velocity = new Vector2(body.velocity.x, Jump);
+            
     }
+
+    
 }
